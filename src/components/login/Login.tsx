ab005aa63ebe6a199944f00a30ae5d2c299a0e28
LarryZhao <larryzhao1994@outlook.com>
2019-03-15 13:40:32 +0800

    login functions

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,10 +1,64 @@
-import * as React from 'react';
+import * as React from 'react'
+import { Input, Icon, Button } from 'antd'
+import {Link} from 'react-router-dom'
+import './login.scss'
+import http from 'src/config/axios'
 
-class Login extends React.Component {
-  public render() {
-    return (
-      <div className="login">login</div>
-    )
-  }
+interface IloginState {
+  account: string,
+  password: string
 }
-export default Login;
+class Login extends React.Component<any, IloginState> {
+  constructor(props) {
+    super(props)
+    this.state = {
+      account:'',
+      password: ''
+    }
+  }
+  submit = async ()=> {
+    const { account, password } = this.state
+    try {
+      await http.post('sign_in/user', {
+        account,
+        password
+      }).then(()=>{
+        console.log('登录成功')
+        this.props.history.push('/')
+      })
+    } catch(e) {
+      throw new Error(e)
+    }
+  }
+  onChangeAccount = (e) => {
+    this.setState({
+      account:e.target.value
+    })
+  }
+  onChangePassword = (e) => {
+    this.setState({
+      password:e.target.value
+    })
+  }
+	public render() {
+    const {password, account} = this.state
+		return (
+			<div className="login">
+				<h1>登录 pomodoro</h1>
+				<Input
+          placeholder="请输入你的用户名"
+          onChange={this.onChangeAccount}
+          value={account}
+					prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+				/>
+				<Input.Password value={password} placeholder="请输入密码"  onChange={this.onChangePassword}/>
+				
+				<Button type="primary" className="loginButton" onClick={this.submit}>
+					登录
+				</Button>
+        <p>如果你没有账号，请立即 <Link to="/signup">注册</Link></p>
+			</div>
+		)
+	}
+}
+export default Login
--- src/components/signup/Signup.tsx
+++ src/components/signup/Signup.tsx
@@ -4,7 +4,6 @@ import {Link} from 'react-router-dom';
 import axios from 'src/config/axios'
 import './signup.scss'
 
-
 interface ISignUpState {
 	account: string,
 	password: string,
